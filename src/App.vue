5bf80c16d5281940f846c91f5931e1bec5f4ee38
FabioBRito_Mablus <fabsman@hotmail.com>
2022-09-29 17:12:18 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+ <MyNavbar :logo="logo_src" :alt="app_name"/>
+  <router-view/>
+  <Footer/>
+</template>
+
+<script>
+import MyNavbar from './components/MyNavbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components: {
+    MyNavbar,
+    Footer
+  },
+  data(){
+    return{
+      logo_src: "/img/logo.png",
+      app_name:"Faça o seu Burger"
+    }
+  }
+}
+
+</script>
+
+<style>
+  * {
+    font-display: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+</style>
